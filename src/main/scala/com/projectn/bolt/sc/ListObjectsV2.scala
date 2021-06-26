7dcdfd741c6b42ca22fdbd76fdb7e3ad3f0f08d7
Amit Warke <aw@projectn.co>
2021-06-25 18:19:31 -0700

    remove credentials provider

--- src/main/scala/com/projectn/bolt/sc/ListObjectsV2.scala
+++ src/main/scala/com/projectn/bolt/sc/ListObjectsV2.scala
@@ -1,7 +1,6 @@
 package com.projectn.bolt.sc
 
 import com.projectn.bolt.BoltS3Client
-import software.amazon.awssdk.auth.credentials.ProfileCredentialsProvider
 import software.amazon.awssdk.services.s3.model.ListObjectsV2Request
 
 import scala.jdk.CollectionConverters._
@@ -10,8 +9,7 @@ object ListObjectsV2 extends App {
 
   var bucketName: String = args(0)
 
-  //val s3 = S3Client.builder.region(Region.US_EAST_2).build
-  val boltS3 = BoltS3Client.builder().credentialsProvider(ProfileCredentialsProvider.create("uat")).build
+  val boltS3 = BoltS3Client.builder.build
 
   val req = ListObjectsV2Request.builder.bucket(bucketName).build
 
